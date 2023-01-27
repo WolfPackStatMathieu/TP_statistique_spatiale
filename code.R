2db7e01f4b9d01326f5bc44e3c8b3156a62fb7e0
WolfPackStatMathieu <mathieu993@hotmail.com>
2023-01-27 15:11:45 +0000

    code avancé

--- code.R
+++ code.R
@@ -27,20 +27,123 @@ commune_bretagne <- fonds_communal %>%
 
 st_crs(commune_bretagne)
 
-plot(commune_bretagne, lwd=0.1, )
+plot(commune_bretagne,
+     lwd=0.1 )
 
 
-plot(st_geometry(commune_bretagne), lwd=0.1)
+plot(st_geometry(commune_bretagne),
+     lwd=0.1)
+require(ggplot2)
+ggplot() + 
+  geom_sf(data= commune_bretagne,
+          fill = "steelblue", col = "grey45")+
+  theme_void()
+
+commune_bretagne <- commune_bretagne %>%
+  mutate(surf2 = st_area(geometry))
+
+
+str(commune_bretagne)
+
+# 11
+all.equal(
+  target = commune_bretagne$surf,
+  round(as.double(commune_bretagne$surf2), 2))
+
 
 commune_bretagne$surf2 <- st_area(commune_bretagne$geometry)
 str(commune_bretagne)
 commune_bretagne$surf2<-commune_bretagne$surf2 * 1000000
-
+#pour supprimer la géometry : 
+# commune_bretagne %>% st_drop_geometry()
 # require(units)
+
+
+
 
 commune_bretagne$surf3 <- units::set_units(st_area(commune_bretagne), "km^2")
 
 str(commune_bretagne)
 
+# 12
 
-dept_bretagne <- 
+depts_bretagne <- commune_bretagne %>%
+  group_by(dep) %>%
+  summarise(
+    surf = sum(surf)
+  )
+str(depts_bretagne)# tjrs objet sf
+plot(depts_bretagne)
+
+# 13 par union des géométries
+
+commune_bretagne %>% st_union() %>% st_geometry() %>% plot()
+
+depts_bretagne_geo <- commune_bretagne %>%
+  group_by(dep) %>%
+  summarise(
+    surf = sum(surf)
+  )
+plot(depts_bretagne_geo)
+str(depts_bretagne_geo)
+
+
+# 14 centroïdes
+centr_depts_bretagne<- depts_bretagne_geo %>%
+  st_centroid()  #warning mais c'est pas une erreur
+
+str(centr_depts_bretagne)
+st_crs(centr_depts_bretagne)
+
+
+plot(depts_bretagne_geo %>% st_geometry())
+plot(centr_depts_bretagne %>% st_geometry(), add = TRUE)
+
+
+ggplot() +
+  geom_sf(data = depts_bretagne_geo)+
+  geom_sf(data= centr_depts_bretagne)+
+  theme_void()
+
+
+
+lib_depts <- data.frame(
+  code = as.character(c(22,29,35,56)),
+  lib=c("Cotes-d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan")
+)
+centr_depts_bretagne <- centr_depts_bretagne %>%
+  left_join(
+    lib_depts,
+    by = c("dep" = "code")
+  )
+
+coords_centr <- st_coordinates(centr_depts_bretagne) %>%
+  bind_cols(
+    centr_depts_bretagne %>%
+      select(dep, lib) %>%
+      st_drop_geometry()
+  )
+str(coords_centr)
+
+
+plot(depts_bretagne_geo %>% st_geometry())
+plot(centr_depts_bretagne %>% st_geometry(), add = TRUE)
+text(coords_centr, labels = coords_centr$lib, adj = 0)
+
+ggplot() +
+  geom_sf(data = depts_bretagne_geo) +
+  geom_sf(data = centr_depts_bretagne)+
+  geom_sf_label(
+    data = centr_depts_bretagne,
+    aes(label = lib), size = 4
+  )  +
+  theme_void()
+
+# 15 - intersection
+
+
+commun
+
+
+
+
